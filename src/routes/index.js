a0f76926454b5d7c7f72834d2acd01cb824146f4
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-08-07 11:46:37 +0100

    bnb reviewed

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,14 @@ import express from 'express';
 const router = express.Router();
 /* GET home page. */
 <<<<<<< HEAD
+<<<<<<< HEAD
 router.get('/', (req, res) => {
 =======
 router.get('/', (req, res, next) => {
 >>>>>>> reviewed_01
+=======
+router.get('/', (req, res) => {
+>>>>>>> bnb reviewed
   res.render('index', { title: 'Express' });
 });
 
