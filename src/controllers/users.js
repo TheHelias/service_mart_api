64b1dd4ec78fe78113a9f6a65fb4ecbc0cca1a0e
Elias Akande <akandetoluwalase@gmail.com>
2019-09-03 16:09:30 +0100

    promise replaced with async-await

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -8,33 +8,26 @@ const { check, body } = require('express-validator');
 const { sanitizeBody, validationResult } = require('express-validator');
 
 const login = [
-// username must be an email
   body('email').isEmail().withMessage('Type in an actual email').normalizeEmail(),
-  // password must be at least 5 chars long
   check('password').isLength({ min: 6 }).withMessage('must be at least 6 characters long'),
-  (req, res, next) => {
-
-    // Extract the validation errors from a request.
-    const errors = validationResult(req);
-
+  async (req, res, next) => {
+    const errors = await validationResult(req);
     if (!errors.isEmpty()) {
-      // There are errors. Render form again with sanitized values/errors messages.
       res.send({ errors: errors.array() });
     } else {
-      authService.authenticate(req.body)
-        .then((token) => {
-          res.send({
-            success: true,
-            data: { token },
-          });
-        })
-        .catch((err) => {
-          res.send({
-            success: false,
-            message: err.message, // not the error handling.
-            // for better error handling visit github repository, link provided below
-          });
+      try {
+        const token = await authService.authenticate(req.body);
+        res.send({
+          success: true,
+          data: { token },
         });
+      }
+      catch (err) {
+        res.send({
+          success: false,
+          message: err.message,
+        });
+      };
     }
   },
 ];
