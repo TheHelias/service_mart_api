28e4107bb22e6d3c3ca3ec108755fb09842d6488
Elias Akande <akandetoluwalase@gmail.com>
2019-09-04 12:30:54 +0100

    completed formerly omitted async-await

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -33,35 +33,29 @@ const login = [
 ];
 const register = [
   body('email').isEmail().withMessage('Type in an actual email').normalizeEmail(),
-  // password must be at least 5 chars long
   check('password').isLength({ min: 6 }).withMessage('must be at least 6 characters long'),
-  (req, res, next) => {
-    const errors = validationResult(req);
-
+  async (req, res, next) => {
+    const errors = await validationResult(req);
     if (!errors.isEmpty()) {
       res.send({ errors: errors.array() })
     } else {
-      userService.getUserByLogin(req.body.email || '')
-        .then((exists) => {
-          if (exists) {
-            return res.send({
-              success: false,
-              message: 'Registration failed. User with this email already registered.',
-            });
-          }
-          const user = {
-            email: req.body.email,
-            password: bcrypt.hashSync(req.body.password, config.saltRounds),
-          };
-          return userService.addUser(user)
-            .then(() => res.send({ success: true, password: user.password }));
+      const exists = await userService.getUserByLogin(req.body.email || '');  
+      if (exists) {
+        return res.send({
+          success: false,
+          message: 'Registration failed. User with this email already registered.',
         });
+      }
+      const user = {
+        email: req.body.email,
+        password: bcrypt.hashSync(req.body.password, config.saltRounds),
+      };
+      return userService.addUser(user)
+        .then(() => res.send({ success: true, password: user.password }));
+        
     }
   },
 ];
-// let {login} = req.body;
-
-
 module.exports = {
   register,
   login,
