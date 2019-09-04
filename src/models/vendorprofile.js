2c660352c4fe2c19599f3e7a256d20c6a15775b9
Elias Akande <akandetoluwalase@gmail.com>
2019-09-04 15:36:55 +0100

    vendor profile model created

---
+++ src/models/vendorprofile.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const vendorProfile = sequelize.define('vendorProfile', {
+    agency_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    agency_email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tel_no: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    service_category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    bio: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+  return vendorProfile;
+};
