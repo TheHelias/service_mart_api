d44a9e5d95ae8a691b7db028c5c8ef2413a042e0
Ibrahim Adeniyi <adeniyiib360@gmail.com>
2019-10-18 03:20:31 +0100

    minor changes to accomodate changes on the client side

--- src/api/v1.0/controllers/users.js
+++ src/api/v1.0/controllers/users.js
@@ -18,9 +18,9 @@ const login = [
     } else {
       try {
         const token = await authService.authenticate(req.body);
-        res.send({
+        res.header('x-auth-token', token).json({
           success: true,
-          data: { token },
+          token: token,
         });
       }
       catch (err) {
--- src/api/v1.0/middlewares/auth.js
+++ src/api/v1.0/middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 const checkAuth = (req, res, next) => {
-  const token = req.headers.token;
+  const token = req.headers['x-access-token'] || req.headers.authorization;
   if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
   jwt.verify(token, config.development.jwtSecret, (err, decoded) => {
     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
