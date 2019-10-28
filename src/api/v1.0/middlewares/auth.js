efe15ccedddf9bb07c5d13ade10d8bdfdcadf5cc
Elias Akande <akandetoluwalase@gmail.com>
2019-10-28 12:16:17 +0100

    jwt heroku setup

--- src/api/v1.0/middlewares/auth.js
+++ src/api/v1.0/middlewares/auth.js
@@ -5,7 +5,7 @@ const config = require('../config/config');
 const checkAuth = (req, res, next) => {
   const token = req.headers['x-access-token'] || req.headers.authorization;
   if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
-  jwt.verify(token, config.development.jwtSecret, (err, decoded) => {
+  jwt.verify(token, config.development.jwtSecret || config.production.jwtSecret, (err, decoded) => {
     if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
     // req.user = {
     //   email: decoded.email,
