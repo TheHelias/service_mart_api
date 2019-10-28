68e2c04080ac9893ea72d44ba976c1cb5119ef53
Elias Akande <akandetoluwalase@gmail.com>
2019-10-28 12:33:25 +0100

    jwt heroku setup

--- src/api/v1.0/config/config.js
+++ src/api/v1.0/config/config.js
@@ -25,7 +25,7 @@ module.exports = {
   production: {
     use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
-    jwtSecret: 'yoitsasecret',
+    jwtSecret: 'JWT_SECRET',
     tokenExpireTime: '6h',
     saltingRounds: 2,
   },
